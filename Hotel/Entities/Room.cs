9727d01df2032b6ac5a6aae29229058b045bd7a3
Alexander Åberg <alexander_aberg1985@msn.com>
2025-03-16 14:48:47 +0100

    Added data to Room class

--- Hotel/Entities/Room.cs
+++ Hotel/Entities/Room.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hotel.Entities
 {
-    internal class Room
+    public class Room
     {
+        public int RoomNumber { get; set; }
+        public Booking Booking { get; set; }
+        public int RoomSize { get; set; }
+        public string BedType { get; set; }
+        public int ExtraBed { get; set; }
     }
 }
