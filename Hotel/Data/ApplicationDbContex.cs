bf4a0e3ada9fa0926bfe58ee7398ec679f785b49
Alexander Åberg <alexander_aberg1985@msn.com>
2025-03-17 14:44:03 +0100

    Created Data folder with ApplicationDbContex class with data

---
+++ Hotel/Data/ApplicationDbContex.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Data
+{
+    internal class ApplicationDbContex : DbContext
+    {
+        public DbSet<Entities.Booking> Bookings { get; set; } = null!;
+        public DbSet<Entities.Guest> Guests { get; set; } = null!;
+        public DbSet<Entities.Room> Rooms { get; set; } = null!;
+    }
+}
