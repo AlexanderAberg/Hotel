6592fe8e52feeac0271774bd980ce3c565e0fe22
Alexander Åberg <alexander_aberg1985@msn.com>
2025-03-19 10:04:19 +0100

    Removed info about other app from Program.cs

--- Hotel/Program.cs
+++ Hotel/Program.cs
@@ -2,11 +2,10 @@
 using Hotel.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using EFCore_Code_First_Tutorial.Data;
 
 namespace Hotel
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
