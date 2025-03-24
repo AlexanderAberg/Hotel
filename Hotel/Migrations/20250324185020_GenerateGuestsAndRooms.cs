314ef323e4da171ce126fbe6cbd98faa0dd8eed8
Alexander Åberg <alexander_aberg1985@msn.com>
2025-03-24 20:05:37 +0100

    Made new migration

---
+++ Hotel/Migrations/20250324185020_GenerateGuestsAndRooms.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Migrations
+{
+    /// <inheritdoc />
+    public partial class GenerateGuestsAndRooms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Guests_Bookings_BookingId",
+                table: "Guests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Bookings_BookingId",
+                table: "Rooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rooms_BookingId",
+                table: "Rooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Guests_BookingId",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "BookingId",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "BookingId",
+                table: "Guests");
+
+            migrationBuilder.RenameColumn(
+                name: "BedType",
+                table: "Rooms",
+                newName: "Bed");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GuestId",
+                table: "Guests",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GuestId",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomNumber",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_GuestId",
+                table: "Bookings",
+                column: "GuestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_RoomNumber",
+                table: "Bookings",
+                column: "RoomNumber");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Guests_GuestId",
+                table: "Bookings",
+                column: "GuestId",
+                principalTable: "Guests",
+                principalColumn: "GuestId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Rooms_RoomNumber",
+                table: "Bookings",
+                column: "RoomNumber",
+                principalTable: "Rooms",
+                principalColumn: "RoomNumber",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Guests_GuestId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Rooms_RoomNumber",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_GuestId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_RoomNumber",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "GuestId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "Bookings");
+
+            migrationBuilder.RenameColumn(
+                name: "Bed",
+                table: "Rooms",
+                newName: "BedType");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookingId",
+                table: "Rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GuestId",
+                table: "Guests",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookingId",
+                table: "Guests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_BookingId",
+                table: "Rooms",
+                column: "BookingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guests_BookingId",
+                table: "Guests",
+                column: "BookingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Guests_Bookings_BookingId",
+                table: "Guests",
+                column: "BookingId",
+                principalTable: "Bookings",
+                principalColumn: "BookingId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Bookings_BookingId",
+                table: "Rooms",
+                column: "BookingId",
+                principalTable: "Bookings",
+                principalColumn: "BookingId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
